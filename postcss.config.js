5b4e1d1833ba93666d61cfa670e7ded8a93a01d3
Marc <ludovicimarc@gmail.com>
2021-08-11 01:34:03 +0200

    finished mini project

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+	plugins: [
+		require('tailwindcss'),
+		require('autoprefixer'),
+		cssnano({
+			preset: 'default',
+		}),
+		purgecss({
+			content: ['./**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+			defaultExtractor: (content) =>
+				content.match(/[\w-/:]+(?<!:)/g) || [],
+		}),
+	],
 }
